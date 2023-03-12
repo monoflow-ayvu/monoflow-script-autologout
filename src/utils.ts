3b19bf2e8f6ed72e1e34401a6cb5cc6dd0f331c1
Fernando Mumbach <fermuch@cryptolab.net>
2023-03-12 23:38:20 +0000

    fixs

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,33 @@
 import { currentLogin, myID } from "@fermuch/monoutils";
 
-export function anyTagMatches(tags: string[]): boolean {
+const MY_TAGS = {
+  loginId: '',
+  lastUpdate: 0,
+  tags: [],
+};
+const MAX_TAG_TIME = 10_000; // ms
+function getMyTags(loginId) {
+  if (MY_TAGS.loginId !== loginId || Date.now() - MY_TAGS.lastUpdate >= MAX_TAG_TIME) {
+    const loginName = loginId || currentLogin() || '';
+    const userTags = env.project?.logins?.find((login) => login.key === loginName || login.$modelId === loginName)?.tags || [];
+    const deviceTags = env.project?.usersManager?.users?.find?.((u) => u.$modelId === myID())?.tags || [];
+    const allTags = [...userTags, ...deviceTags];
+
+    platform.log('[GPS] updating tags store');
+    MY_TAGS.loginId = loginId;
+    MY_TAGS.lastUpdate = Date.now();
+    MY_TAGS.tags = allTags;
+  }
+
+  return MY_TAGS.tags;
+}
+
+export function anyTagMatches(tags: string[], loginId?: string): boolean {
   // we always match if there are no tags
   if (!tags || tags.length === 0) return true;
 
-  const userTags = env.project?.logins?.find((login) => login.key === currentLogin())?.tags || [];
-  const deviceTags = env.project?.usersManager?.users?.find?.((u) => u.$modelId === myID())?.tags || [];
-  const allTags = [...userTags, ...deviceTags];
-
-  console.warn('all tags: ', allTags)
+  const loginName = loginId || currentLogin() || '';
+  const allTags = getMyTags(loginName);
 
   return tags.some((t) => allTags.includes(t));
 }
